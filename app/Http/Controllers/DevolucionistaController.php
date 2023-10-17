e82cb351e0d3af136718df7516483cead32beb59
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-10-17 17:54:01 -0300

    criação do request e resource de devolucionista

--- app/Http/Controllers/DevolucionistaController.php
+++ app/Http/Controllers/DevolucionistaController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DevolucionistaRequest;
+use App\Http\Resources\DevolucionistaResource;
+use App\Models\Devolucionista;
 use Illuminate\Http\Request;
 
 class DevolucionistaController extends Controller
 {
-    //
+    private $devolucionista;
+
+    public function __construct(Devolucionista $devolucionista)
+    {
+        $this->devolucionista = $devolucionista;
+    }
+
+    public function index(){
+        return DevolucionistaResource::collection(
+            $this->devolucionista->all()
+        );
+    }
+
+    public function store(DevolucionistaRequest $request){
+        $devolucionistaExiste = $this->devolucionista->where('nome', $request->nome)
+            ->where('data_nascimento', $request->data_nascimento)
+            ->first();
+
+        if($devolucionistaExiste){
+
+        }
+    }
 }
